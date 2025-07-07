c44a1da428a037a8d0f17bd134a9b60c4684f871
J. Nick Koston <nick@koston.org>
2025-07-07 07:40:48 -0500

    Release 0.68.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "cryptography>=39.0.0",
         "pyOpenSSL>=23.0.0",
     ],
-    version="0.68.0",
+    version="0.68.1",
     description="A library to communicate with Switchbot",
     long_description=long_description,
     long_description_content_type="text/markdown",
